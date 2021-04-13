c2bb895af5a85c8918a9c785591693cd9ef3fce3
Graywolf <akosheev@rambler.ru>
2021-04-13 13:33:09 +0300

    Add models and admin class.

--- personnel/admin.py
+++ personnel/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from .models import Subdivision, Position, Employee
 
-# Register your models here.
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    fields = ['fio', 'date_of_birth', 'photo', 'subdivision', 'position']
+
+
+@admin.register(Subdivision)
+class SubdivisionAdmin(MPTTModelAdmin):
+    fields = ['name', 'parent', 'director']
+
+
+@admin.register(Position)
+class PositionAdmin(MPTTModelAdmin):
+    fields = ['name', 'parent']
--- personnel/models.py
+++ personnel/models.py
@@ -1,26 +1,48 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey
 
 
-class Subdivision(models.Model):
-    pass
+class Subdivision(MPTTModel):
+    name = models.CharField(max_length=255, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE,
+                            null=True, blank=True, related_name='children')
+    director = models.OneToOneField('Employee', on_delete=models.SET_NULL,
+                                    related_name='directors', verbose_name=_('Начальник'))
 
     class Meta:
         verbose_name = _('Подразделение')
         verbose_name_plural = _('Подразделения')
 
+    def __str__(self):
+        return self.name
 
-class Position(models.Model):
-    pass
+
+class Position(MPTTModel):
+    name = models.CharField(max_length=255, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE,
+                            null=True, blank=True, related_name='children')
 
     class Meta:
         verbose_name = _('Должность')
         verbose_name_plural = _('Должности')
 
+    def __str__(self):
+        return self.name
+
 
 class Employee(models.Model):
-    pass
+    fio = models.CharField(max_length=255, verbose_name=_('ФИО'))
+    date_of_birth = models.DateField(verbose_name=_('Дата рождения'))
+    photo = models.ImageField(upload_to='employee/photo', verbose_name=_('Фото'))
+    subdivision = models.ForeignKey(Subdivision, on_delete=models.SET_NULL,
+                                    related_name='staff', verbose_name=_('Подразделение'))
+    position = models.ForeignKey(Position, on_delete=models.SET_NULL,
+                                  related_name='staff', verbose_name=_('Должность'))
 
     class Meta:
         verbose_name = _('Сотрудник')
         verbose_name_plural = _('Сотрудники')
+
+    def __str__(self):
+        return self.fio
